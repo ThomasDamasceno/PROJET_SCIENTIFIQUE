8833f7f0119b7539bf7d3a665f3c9b1abb77fa76
emileargte <61014429+emileargte@users.noreply.github.com>
2021-01-06 11:03:12 +0100

    Add files via upload

--- feu.sql
+++ feu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 05 jan. 2021 à 15:46
+-- Généré le : mer. 06 jan. 2021 à 10:02
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -30,38 +30,20 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `feu`;
 CREATE TABLE IF NOT EXISTS `feu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `intensite` int(11) NOT NULL,
-  `lat` decimal(10,8) NOT NULL,
-  `lon` decimal(10,8) NOT NULL,
+  `intensite` tinyint(4) UNSIGNED NOT NULL,
+  `lat` double NOT NULL,
+  `lon` double NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `lat` (`lat`),
   UNIQUE KEY `lon` (`lon`)
-) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=154 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `feu`
 --
 
 INSERT INTO `feu` (`id`, `intensite`, `lat`, `lon`) VALUES
-(1, 50, '45.74500000', '4.82000000'),
-(2, 210, '45.74200000', '4.89000000'),
-(3, 15, '45.74800000', '4.85700000'),
-(4, 255, '45.74100000', '4.85200000'),
-(5, 40, '45.76933000', '4.85503000'),
-(46, 37, '45.74462690', '4.84422511'),
-(47, 18, '45.76200993', '4.88930954'),
-(48, 25, '45.72615823', '4.87785043'),
-(49, 43, '45.76829747', '4.89103924'),
-(50, 46, '45.76554799', '4.85210898'),
-(51, 15, '45.74709723', '4.82253483'),
-(52, 3, '45.74473295', '4.88088243'),
-(53, 30, '45.78295201', '4.86779815'),
-(54, 29, '45.77867489', '4.79145324'),
-(55, 26, '45.74098123', '4.83538129'),
-(56, 47, '45.77079401', '4.81565734'),
-(57, 23, '45.75892363', '4.84888301'),
-(58, 16, '45.74898322', '4.84366337'),
-(59, 11, '45.72851899', '4.83236573');
+(153, 38, 45.739499124736554, 4.813514074753513);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
